feb31f51f2ed590383281b43bc9eeb1a377138e7
譚永祺 <martintan20030922@gmail.com>
2025-06-22 00:09:39 +0800

    modified

--- src/main/java/com/gui/Frame.java
+++ src/main/java/com/gui/Frame.java
@@ -15,8 +15,8 @@ public class Frame extends JFrame {
         setSize(WIDTH, HEIGHT);
         setLocationRelativeTo(null);
 
-        //panel = new HomePanel(WIDTH, HEIGHT, e -> switchToRoom());
-        panel = new GamePanel(WIDTH, HEIGHT, null);
+        panel = new HomePanel(WIDTH, HEIGHT, e -> switchToRoom());
+        //panel = new GamePanel(WIDTH, HEIGHT, null);
         add(panel);
     }
 
